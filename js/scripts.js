2502ea9bb9d0dd21b8267e8e3d21b6a3756b0125
Regina Nurieva <regginurieva@gmail.com>
2018-08-21 16:28:01 -0700

    add spec for input verification(alert if input is a not a number)

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ event.preventDefault();
   $(".output").text("");
   var userInputNum = $("#inputBox").val();
   var convertedInput = parseInt(userInputNum);
+  if(isNaN(convertedInput)){
+  alert ("Stop doing what you're doing!!")
+  }
   for (var i=0; i <= convertedInput; i++){
     if (i % 3 === 0 && i!=0) {
       $(".output").append("<li>I'm sorry, Dave. I'm afraid I can't do that!</li>");
@@ -11,11 +14,9 @@ event.preventDefault();
     $(".output").append("<li>Boop!</li>");
   } else if (i.toString().includes("0")) {
     $(".output").append("<li>Beep!</li>");
-  } else {
+   } else {
     $(".output").append("<li>"+i+"</li>");
-    }
   }
-
+  }
 });
-
 });
